d4e288b29fb5807ab0817787b847cbc0b9835f3b
lucachecha <82596990+lucachecha@users.noreply.github.com>
2021-09-13 19:55:36 -0300

    Precio final

--- LoginPage/Carrito/Carrito.aspx.cs
+++ LoginPage/Carrito/Carrito.aspx.cs
@@ -24,6 +24,18 @@ namespace LoginPage.Carrito
                 Response.Redirect("~/Login/LoginPage.aspx");
             }
 
+            if (!IsPostBack)
+            {
+
+                MySqlCommand comando2 = new MySqlCommand("SELECT nombre, cantidad, sucursal, precio_unitario, precio_final FROM carrito ", Conexion.ObtenerConexion());
+                MySqlDataAdapter DA = new MySqlDataAdapter(comando2);
+                DataSet DS = new DataSet();
+                DA.Fill(DS);
+                GridView1.DataSource = DS.Tables[0];
+                GridView1.DataBind();
+
+            }
+
 
         }
 
@@ -75,6 +87,28 @@ namespace LoginPage.Carrito
             comando3.ExecuteNonQuery();
 
             GridView1.DataBind();
+        }
+
+        protected void Actualizar_Click(object sender, EventArgs e)
+        {
+
+            int[] cantidad = new int[GridView1.Rows.Count];
+            int[] precio = new int[GridView1.Rows.Count];
+
+            for (int counter = 0; counter < (GridView1.Rows.Count); counter++)
+            {
+
+                TextBox txtValor = GridView1.Rows[counter].Cells[1].FindControl("txtQuantity") as TextBox;
+
+                cantidad[counter] = Convert.ToInt32(txtValor.Text);
+
+                precio[counter] = Convert.ToInt32(GridView1.Rows[counter].Cells[3].Text);
+
+                GridView1.Rows[counter].Cells[4].Text = Convert.ToString(cantidad[counter] * precio[counter]);
+
+
+            }
+
         }
     }
 }
--- LoginPage/Carrito/Carrito.aspx.designer.cs
+++ LoginPage/Carrito/Carrito.aspx.designer.cs
@@ -58,5 +58,23 @@ namespace LoginPage.Carrito
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Remover;
+
+        /// <summary>
+        /// Control Actualizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Actualizar;
+
+        /// <summary>
+        /// Control PrecioTotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PrecioTotal;
     }
 }
