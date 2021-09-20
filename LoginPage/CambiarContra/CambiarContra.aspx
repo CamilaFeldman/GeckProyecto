aefe3d63f26685f1f088b9957819a9fefaccfffe
ItsGian <gianmarquinho2002@gmail.com>
2021-09-20 18:17:59 -0300

    Intento 1 password recovery

---
+++ LoginPage/CambiarContra/CambiarContra.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CambiarContra.aspx.cs" Inherits="LoginPage.CambiarContra.CambiarContra" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3 class="formulario-mail">Cambie su contraseña</h3>
+        <h3 class="formulario-mail">Ingrese su correo electrónico</h3>        
+        <div class="formulario-barra"><asp:TextBox ID="TextBox1" runat="server" class="completar-email" AutoComplete="Off" OnTextChanged="TextBox1_TextChanged"></asp:TextBox></div>
+        <asp:Button ID="Button1" runat="server" Text="Mandar mail de confirmación" class="login-rectangulo" OnClick="Button1_Click"/>
+        <br />
+        <asp:Label ID="Label1" runat="server"/>
+    </form>
+</body>
+</html>
---
+++ LoginPage/CambiarContra/CambiarContra.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Drawing;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginPage.CambiarContra
+{
+    public partial class CambiarContra : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = string.Empty;
+            string password = string.Empty;
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT nombre FROM usuarios WHERE mail = @Email"))
+                {
+                    cmd.Parameters.AddWithValue("@Email", TextBox1.Text.Trim());
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        if (sdr.Read())
+                        {
+                            username = sdr["Username"].ToString();
+                            password = sdr["Password"].ToString();
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            if (!string.IsNullOrEmpty(password))
+            {
+                MailMessage mm = new MailMessage("sender@gmail.com", TextBox1.Text.Trim());
+                mm.Subject = "Password Recovery";
+                mm.Body = string.Format("Hola, {0},<br /><br />Tu contraseña es {1}.<br /><br />Muchas gracias.", username, password);
+                mm.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential NetworkCred = new NetworkCredential();
+                NetworkCred.UserName = "sender@gmail.com";
+                NetworkCred.Password = "<Password>";
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 587;
+                smtp.Send(mm);
+                Label1.ForeColor = Color.Green;
+                Label1.Text = "La contraseña fue enviada a su mail.";
+            }
+            else
+            {
+                Label1.ForeColor = Color.Red;
+                Label1.Text = "Este mail no existe.";
+            }
+        }
+    }
+}
