d7dbc18c29e8833fd11a4e3bfec6866c3ba673ce
lucachecha <82596990+lucachecha@users.noreply.github.com>
2021-09-29 18:44:32 -0300

    fecha en productos

--- LoginPage/Productos/ProductosDAL.cs
+++ LoginPage/Productos/ProductosDAL.cs
@@ -13,7 +13,7 @@ namespace LoginPage.Productos
         public static int Agregar(ProductosBLL pProductos)
         {
             int retorno = 0;
-            MySqlCommand comando2 = new MySqlCommand(string.Format("INSERT into producto_especifico(nombre, precio, marcas, stock, proveedor, cant_min, cant_max, presentacion, sucursal) values('{0}','{1}' , '{2}', '{3}','{4}' , '{5}', '{6}', '{7}', '{8}')", pProductos.Nombre, pProductos.Precio, pProductos.Marcas, pProductos.Stock, pProductos.Proveedor, pProductos.CantidadMinima, pProductos.CantidadMaxima, pProductos.Presentacion, pProductos.Sucursal), Conexion.ObtenerConexion());
+            MySqlCommand comando2 = new MySqlCommand(string.Format("INSERT into producto_especifico(nombre, precio, marcas, stock, proveedor, cant_min, cant_max, presentacion, sucursal, fecha) values('{0}','{1}' , '{2}', '{3}','{4}' , '{5}', '{6}', '{7}', '{8}', '{9}')", pProductos.Nombre, pProductos.Precio, pProductos.Marcas, pProductos.Stock, pProductos.Proveedor, pProductos.CantidadMinima, pProductos.CantidadMaxima, pProductos.Presentacion, pProductos.Sucursal, pProductos.Fecha), Conexion.ObtenerConexion());
 
             retorno = comando2.ExecuteNonQuery();
 
@@ -24,10 +24,10 @@ namespace LoginPage.Productos
         public static bool CompararProducto(string idProducto, string idProveedor)
         {
 
-            MySqlCommand command = new MySqlCommand(/*string.Format("SELECT * from usuarios where(email, password) values('{0}','{1}')", pCliente.Mail, pCliente.Password), Conexion.ObtenerConexion()*/);
+            MySqlCommand command = new MySqlCommand();
 
             command.Connection = Conexion.ObtenerConexion();
-            command.CommandText = "select * from producto_especifico where (nombre=@user && proveedor=@proveedor)";
+            command.CommandText = "SELECT * FROM producto_especifico WHERE (nombre=@user && proveedor=@proveedor)";
             command.Parameters.AddWithValue("@user", idProducto);
             command.Parameters.AddWithValue("@proveedor", idProveedor);
             command.CommandType = CommandType.Text;
@@ -53,16 +53,15 @@ namespace LoginPage.Productos
         public static ProductosBLL Editar(string idProducto, string idProveedor)
         {
 
-            MySqlCommand command = new MySqlCommand(/*string.Format("SELECT * from usuarios where(email, password) values('{0}','{1}')", pCliente.Mail, pCliente.Password), Conexion.ObtenerConexion()*/);
+            MySqlCommand command = new MySqlCommand();
 
             command.Connection = Conexion.ObtenerConexion();
-            command.CommandText = "select * from producto_especifico where (nombre=@user && proveedor=@proveedor)";
+            command.CommandText = "SELECT * FROM producto_especifico WHERE (nombre=@user && proveedor=@proveedor)";
             command.Parameters.AddWithValue("@user", idProducto);
             command.Parameters.AddWithValue("@proveedor", idProveedor);
             command.CommandType = CommandType.Text;
 
-            // try
-            //{
+         
             MySqlDataReader reader;
 
             reader = command.ExecuteReader();
@@ -87,12 +86,6 @@ namespace LoginPage.Productos
                 return Nada;
             }
 
-            // }
-            // catch (Exception)
-            //{
-            //    return "";
-            //}
-
 
         }
         public static void Eliminar(string lNombre, string lProveedor)
@@ -112,7 +105,7 @@ namespace LoginPage.Productos
             MySqlCommand command = new MySqlCommand(/*string.Format("SELECT * from usuarios where(email, password) values('{0}','{1}')", pCliente.Mail, pCliente.Password), Conexion.ObtenerConexion()*/);
 
             command.Connection = Conexion.ObtenerConexion();       
-            command.CommandText = "UPDATE producto_especifico SET nombre=@nombre, precio=@precio, marcas=@marcas, stock=@stock, proveedor=@proveedor, cant_min=@cant_min, cant_max=@cant_max, presentacion=@presentacion, sucursal=@sucursal where (id=@id)";
+            command.CommandText = "UPDATE producto_especifico SET nombre=@nombre, precio=@precio, marcas=@marcas, stock=@stock, proveedor=@proveedor, cant_min=@cant_min, cant_max=@cant_max, presentacion=@presentacion, sucursal=@sucursal, fecha=@fecha WHERE (id=@id)";
             command.Parameters.AddWithValue("@id", pProductos.Id);
             command.Parameters.AddWithValue("@nombre", pProductos.Nombre);
             command.Parameters.AddWithValue("@precio", pProductos.Precio);
@@ -123,36 +116,14 @@ namespace LoginPage.Productos
             command.Parameters.AddWithValue("@cant_max", pProductos.CantidadMaxima);
             command.Parameters.AddWithValue("@presentacion", pProductos.Presentacion);
             command.Parameters.AddWithValue("@sucursal", pProductos.Sucursal);
+            command.Parameters.AddWithValue("@fecha", pProductos.Fecha);
             command.CommandType = CommandType.Text;
 
 
             MySqlDataReader reader;
 
             reader = command.ExecuteReader();
-            /*if (reader.Read())
-            {
-                string Nombre = reader["nombre"].ToString();
-                string Proveedor = reader["proveedor"].ToString();
-                string Precio = reader["precio"].ToString();
-                string Categoria = reader["marcas"].ToString();
-                string Stock = reader["stock"].ToString();
-                string CantidadMinima = reader["cant_min"].ToString();
-                string CantidadMaxima = reader["cant_max"].ToString();
-                string PresentacionComercial = reader["presentacion"].ToString();
-                string Sucursal = reader["sucursal"].ToString();
-
-                ProductosBLL recProducto = new ProductosBLL(Nombre, Proveedor, Precio, Categoria, Stock, CantidadMinima, CantidadMaxima, PresentacionComercial, Sucursal);
-
-                return recProducto;
-            }
-            else
-            {
-                ProductosBLL Nada = new ProductosBLL();
-                return Nada;
-            }
-            */
-
-
+            
         }
     }
 
