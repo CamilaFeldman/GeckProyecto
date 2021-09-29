c0cb1f84705da43d008ba482f5e3861a72d714af
Camila <camilacatalinafeldman@gmail.com>
2021-09-29 00:06:06 -0300

    Anda la site master

--- LoginPage/Site.Master.designer.cs
+++ LoginPage/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace LoginPage
     {
 
         /// <summary>
-        /// MainContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-
-        /// <summary>
         /// Editable control.
         /// </summary>
         /// <remarks>
@@ -31,5 +22,14 @@ namespace LoginPage
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlAnchor Editable;
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
